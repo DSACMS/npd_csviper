3ab56e297577673052542b729a14a5c5961e239f
ftrotter-gov <frederick.trotter@cms.hhs.gov>
2025-08-09 17:11:06 -0400

    renaming

--- run_test_compile.sh
+++ run_test_compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /Users/ftrotter/gitgov/ftrotter/csviper && python -m csviper full-compile --from_csv test_data.csv --overwrite_previous
+cd /Users/ftrotter/gitgov/DSACMS/npd_csviper && python -m npd_csviper full-compile --from_csv test_data.csv --overwrite_previous
